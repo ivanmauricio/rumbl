7c934c32198a0af193e99bf45d67d60ad63258ac
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-05-18 10:49:14 +0100

    End of chapter 6.

--- web/router.ex
+++ web/router.ex
@@ -20,11 +20,11 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
-    resources "/videos", VideoController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user] # Use the default browser stack
+
+    resources "/videos", VideoController
+  end
 end
